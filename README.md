3f97639a34b61c91cbd5ff61c5a340cfb7d580d1
bierdosenhalter <529651+bierdosenhalter@users.noreply.github.com>
2020-07-13 16:29:19 +0200

    added index for joindate and playtime added config values

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 #PlaytimeNameColor
 
 A spigot plugin where players are rewarded with additional name colors for their joining date and playing time.
+
+#Sources
+
+Using source code from:
+- PlayTime by F64_Rx
+- JoinDate by XSilverFalconX
--- src/main/java/org/zerobzerot/playtimenamecolor/PlaytimeNameColor.java
+++ src/main/java/org/zerobzerot/playtimenamecolor/PlaytimeNameColor.java
@@ -1,17 +1,55 @@
 package org.zerobzerot.playtimenamecolor;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Statistic;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PlaytimeNameColor extends JavaPlugin {
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public final class PlaytimeNameColor extends JavaPlugin implements Listener {
+    final List<String> defaultColors = Stream.of(ChatColor.GRAY, ChatColor.WHITE, ChatColor.GREEN, ChatColor.BLUE, ChatColor.DARK_PURPLE, ChatColor.GOLD, ChatColor.RED, ChatColor.YELLOW, ChatColor.AQUA).map(ChatColor::toString).collect(Collectors.toList());
+    List<String> colors;
+
+    int maxPlaytime;
+    int maxJoindate;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getConfig().addDefault("colors", defaultColors);
+        getConfig().addDefault("playtime", 8);
+        getConfig().addDefault("joindate", 365);
 
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+
+        colors = getConfig().getStringList("colors");
+        maxPlaytime = getConfig().getInt("playtime");
+        maxJoindate = getConfig().getInt("joindate");
+
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Bukkit.getScheduler().runTaskAsynchronously(this, () -> {
+            Player player = event.getPlayer();
+
+            double joindate = (int) ((System.currentTimeMillis() - player.getFirstPlayed()) / 1000L);
+            double playtime = player.getStatistic(Statistic.PLAY_ONE_TICK) / 20d;
+
+            int indexPlaytime = (int) Math.floor((colors.size() - 1) * Math.min(1d, playtime / (maxPlaytime * 60d * 60d)));
+            int indexJoindate = (int) Math.floor((colors.size() - 1) * Math.min(1d, joindate / (maxJoindate * 24d * 60d * 60d)));
+
+            int index = Math.min(indexJoindate, indexPlaytime);
+
+            player.setDisplayName(colors.get(index) + player.getName() + ChatColor.RESET);
+        });
     }
 }
